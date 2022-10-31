c3fea7a5116417ef17895816b52a1d5458abfd22
DouglasHdezT <dohernandez@uca.edu.sv>
2022-10-31 08:21:59 -0600

    :art: Add REST class

--- [2022-10-24] Clase XX - What do you think/wdyt-uca-api/app.js
+++ [2022-10-24] Clase XX - What do you think/wdyt-uca-api/app.js
@@ -2,19 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require("./config/mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const apiRouter = require("./routes/api/index.router");
 
 var app = express();
 
-app.use(logger('dev'));
+mongoose.connect();
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/api", apiRouter);
 
 module.exports = app;
